f74e860e3644d26c6cc816f8be578289f229c9b0
Samuel Camilo <itscamilo@outlook.com>
2023-10-23 13:40:45 -0300

    feat: added golang hello world with tests

---
+++ learn-go-with-tests/1.hello-world/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const messagePrefix string = "Hello, "
+
+func Hello(s string) string {
+	if strings.EqualFold(s, "") {
+		return messagePrefix + "World!"
+	}
+
+	return "Hello, " + s + "!" 
+}
+
+func main() {
+	fmt.Println(Hello("Camilo"))
+}
---
+++ learn-go-with-tests/1.hello-world/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Camilo")
+		want := "Hello, Camilo!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
